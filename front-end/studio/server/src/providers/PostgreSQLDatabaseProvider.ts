c27235b13387d1f8efcc344ec7ae7cef83c138bb
Enda Phelan <ephelan@redhat.com>
2022-04-20 13:41:20 +0100

    fix: normalize data

--- front-end/studio/server/src/providers/PostgreSQLDatabaseProvider.ts
+++ front-end/studio/server/src/providers/PostgreSQLDatabaseProvider.ts
@@ -1,8 +1,4 @@
-import {
-  NewRuleset,
-  Ruleset,
-  UpdateRuleset,
-} from "../models";
+import { NewRuleset, Ruleset, UpdateRuleset } from "../models";
 import { DatabaseProvider } from "./DatabaseProvider";
 import { Client } from "pg";
 
@@ -39,27 +35,35 @@ export class PostgreSQLDatabaseProvider implements DatabaseProvider {
   async CreateRuleset(ruleset: NewRuleset) {
     const query =
       "INSERT INTO rulesets(display_name, config) VALUES($1, $2) returning *";
-	     
-      await this.db.query(query, [ruleset.name, ruleset.config]);
+
+    await this.db.query(query, [ruleset.name, ruleset.config]);
   }
 
   async UpdateRuleset(id: number, ruleset: UpdateRuleset) {
-	const query = `UPDATE rulesets SET
+    const query = `UPDATE rulesets SET
 	display_name = COALESCE($1, display_name),
 	config = COALESCE($2, config)
 WHERE id = $3`;
 
-   this.db.query(query, [ruleset.name, ruleset.config, id]);
+    this.db.query(query, [ruleset.name, ruleset.config, id]);
   }
 
   async GetRulesetById(id: number): Promise<Ruleset> {
     const query = "SELECT * FROM rulesets WHERE id = $1 LIMIT 1";
 
-    const results = await this.db.query<Ruleset>(query, [id]);
+    const results = await this.db.query(query, [id]);
     if (results.rowCount === 0) {
       return null;
     }
 
-    return Promise.resolve(results.rows[0]);
+    const row = results.rows[0];
+    
+    const ruleset: Ruleset = {
+      id: row.id,
+      name: row.display_name,
+      config: row.config,
+    };
+
+    return Promise.resolve(ruleset);
   }
 }
