b8c95792d7879a7e7c7b608b000bcc91c3142d23
Enda Phelan <ephelan@redhat.com>
2022-04-20 12:18:07 +0100

    feat: add database providers

---
+++ front-end/studio/server/src/providers/PostgreSQLDatabaseProvider.ts
@@ -0,0 +1,65 @@
+import {
+  NewRuleset,
+  Ruleset,
+  UpdateRuleset,
+} from "../models";
+import { DatabaseProvider } from "./DatabaseProvider";
+import { Client } from "pg";
+
+/**
+ * Database provider for fetching and inserting rulesets to a PostgreSQL database
+ */
+export class PostgreSQLDatabaseProvider implements DatabaseProvider {
+  private db: Client;
+  constructor(client: Client) {
+    this.db = client;
+  }
+
+  async ListRulesets(): Promise<Ruleset[]> {
+    const query = "SELECT * FROM rulesets";
+    const results = await this.db.query(query);
+
+    if (results.rowCount === 0) {
+      return [];
+    }
+
+    const rulesets: Ruleset[] = results.rows.map(
+      (row: { id: number; display_name: string; config: string }) => {
+        return {
+          id: row.id,
+          name: row.display_name,
+          config: row.config,
+        };
+      }
+    );
+
+    return Promise.resolve(rulesets);
+  }
+
+  async CreateRuleset(ruleset: NewRuleset) {
+    const query =
+      "INSERT INTO rulesets(display_name, config) VALUES($1, $2) returning *";
+	     
+      await this.db.query(query, [ruleset.name, ruleset.config]);
+  }
+
+  async UpdateRuleset(id: number, ruleset: UpdateRuleset) {
+	const query = `UPDATE rulesets SET
+	display_name = COALESCE($1, display_name),
+	config = COALESCE($2, config)
+WHERE id = $3`;
+
+   this.db.query(query, [ruleset.name, ruleset.config, id]);
+  }
+
+  async GetRulesetById(id: number): Promise<Ruleset> {
+    const query = "SELECT * FROM rulesets WHERE id = $1 LIMIT 1";
+
+    const results = await this.db.query<Ruleset>(query, [id]);
+    if (results.rowCount === 0) {
+      return null;
+    }
+
+    return Promise.resolve(results.rows[0]);
+  }
+}
