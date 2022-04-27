1d7a9f66a77ff75e85664d73cb5dbc9133c757b7
Enda Phelan <ephelan@redhat.com>
2022-04-27 17:51:03 +0100

    feat: add CORS bypass filter

--- back-end/spectral-api/src/index.ts
+++ back-end/spectral-api/src/index.ts
@@ -4,10 +4,33 @@ if (process.env.NODE_ENV !== 'production') {
 import fastify from 'fastify';
 import { configureRoutes } from './routes';
 import createService from './services/createService';
+import cors, { FastifyCorsOptions } from '@fastify/cors';
 
 // Run the server!
 const start = async () => {
   const server = fastify({ logger: process.env.NODE_ENV !== "production" });
+
+  // specify which origins can access the server
+  server.register(cors, () => {
+    return (req, callback) => {
+      const origin = req.headers.origin
+
+      const allowedOrigins = process.env.ALLOWED_ORIGINS;
+      const options: FastifyCorsOptions = { origin: false };
+      if (!allowedOrigins) {
+        callback(null, options);
+      }
+      const allowedOriginList = allowedOrigins.split(',');
+      if (!allowedOriginList.length) {
+        callback(null, options);
+      }
+      
+      if (allowedOriginList.includes(origin)) {
+        options.origin = true;
+      }
+      callback(null, options);
+    } 
+  });
 
   const spectralService = createService();
 
